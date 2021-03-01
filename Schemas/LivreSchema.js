c2b44a0bd712967e5d66e642ab1a4037aa22ba7d
sofienkarka <sofien.gallas@gmail.com>
2021-03-01 15:14:24 +0100

    update attribute image

---
+++ Schemas/LivreSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema =mongoose.Schema
+const LivreSchema =new Schema({
+    titre:{type:String,required:true},
+    auteur:{type:String,required: true},
+    maisonEdition:{type:String,required: true},
+    anneeEdition:{type:Number,required:true},
+    categorie:{type:String,required:true},
+    langue:{type:String,required:true},
+    images: {type:String,required:true},
+    stock:{type:Number,required:true},
+    etat:{type:String,required:true}
+})
+module.exports= mongoose.model('Livres', LivreSchema);
