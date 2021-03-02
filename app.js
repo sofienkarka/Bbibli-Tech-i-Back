4876cf425483a35e7c321e315e9b8753ccdb8725
wafaslim <js.wafa@gmail.com>
2021-03-02 11:09:15 +0100

    login et register

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app=express();
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const authAPI = require ('./Routes/authAPI')
 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/auth', authAPI);
 
 
 
